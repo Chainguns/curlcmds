bdfd0bb8b2165fb6bdc1793c4a17d4f569c4dfd9
Daniel <danielhirshson@gmail.com>
2022-02-01 13:10:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from setup import URLS, HEADERS, D
+from dynamic_setup import URLS, HEADERS, D
 import os
 import sys
 
@@ -7,8 +7,10 @@ for url in URLS:
     proxy = "10.170.6.5:8080"
     # res = requests.get(url, data=D[count],headers=HEADERS )
     cmd = "curl -s -X PATCH " + "-x " + proxy + " -H " + HEADERS[0] + " -H " + "'User-Agent:'" + " " + "-H " + HEADERS[1]\
-          + " -k " + url + sys.argv[1] + " -d " + D[count]
+          + " -k " + url + " -d " + sys.argv[1] + D[count]
+
     print(cmd)
-    count += 1
+    if count != len(D):
+        count += 1
     t = os.popen(cmd, 'r', 1)
     t.close()
